d16d2a016f68d49b3c9836761fa8df9dcf81e56d
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-14 11:46:03 -0400

    built max number program using vanilla c#

--- moshBeginner/Program.cs
+++ moshBeginner/Program.cs
@@ -298,6 +298,27 @@
 
 //5- Write a program and ask the user to enter a series of numbers separated by comma.Find the maximum of the numbers and display it on the console.For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
 
+//using System;
+//using System.Linq;
+
+//class MainClass
+//{
+
+//    // Main Method 
+//    public static void Main()
+//    {
+//        string number;
+
+//        Console.WriteLine("Please give me a series of numbers, comma seperated ");
+
+//        number = Console.ReadLine();
+
+//        int[] array = number.Split(',').Select(str => int.Parse(str)).ToArray();
+
+//        Console.WriteLine(array.Max());
+//    }
+//}
+
 using System;
 using System.Linq;
 
@@ -312,9 +333,17 @@ class MainClass
         Console.WriteLine("Please give me a series of numbers, comma seperated ");
 
         number = Console.ReadLine();
-        
+
         int[] array = number.Split(',').Select(str => int.Parse(str)).ToArray();
 
-        Console.WriteLine(array.Max());
+        int maxNumber = 0;
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] > maxNumber)
+            {
+                maxNumber = array[i];
+            }
+        }
+        Console.WriteLine(maxNumber);
     }
 }
