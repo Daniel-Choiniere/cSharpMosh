0d3fd4d33c213998efb83c8d7a47981fd1f0430c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-14 13:09:04 -0400

    problemset 3 questions 1 -5 uplaoded

--- moshBeginner/Program.cs
+++ moshBeginner/Program.cs
@@ -319,31 +319,58 @@
 //    }
 //}
 
-using System;
-using System.Linq;
+//using System;
+//using System.Linq;
 
-class MainClass
-{
+//class MainClass
+//{
 
-    // Main Method 
-    public static void Main()
-    {
-        string number;
+//    // Main Method 
+//    public static void Main()
+//    {
+//        string number;
 
-        Console.WriteLine("Please give me a series of numbers, comma seperated ");
+//        Console.WriteLine("Please give me a series of numbers, comma seperated ");
 
-        number = Console.ReadLine();
+//        number = Console.ReadLine();
 
-        int[] array = number.Split(',').Select(str => int.Parse(str)).ToArray();
+//        int[] array = number.Split(',').Select(str => int.Parse(str)).ToArray();
 
-        int maxNumber = 0;
-        for (int i = 0; i < array.Length - 1; i++)
-        {
-            if (array[i] > maxNumber)
-            {
-                maxNumber = array[i];
-            }
-        }
-        Console.WriteLine(maxNumber);
-    }
-}
+//        int maxNumber = 0;
+//        for (int i = 0; i < array.Length - 1; i++)
+//        {
+//            if (array[i] > maxNumber)
+//            {
+//                maxNumber = array[i];
+//            }
+//        }
+//        Console.WriteLine(maxNumber);
+//    }
+//}
+
+//PROBLEM SET 3
+//1- When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information.
+
+//If no one likes your post, it doesn't display anything.
+//If only one person likes your post, it displays: [Friend's Name] likes your post.
+//If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
+//If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number of Other People] others like your post.
+//Write a program and continuously ask the user to enter different names, until the user presses Enter (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
+
+
+
+
+
+//2- Write a program and ask the user to enter their name.Use an array to reverse the name and then store the result in a new string. Display the reversed name on the console.
+
+
+
+//3- Write a program and ask the user to enter 5 numbers.If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+
+
+
+//4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates.Display the unique numbers that the user has entered.
+
+
+
+//5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
