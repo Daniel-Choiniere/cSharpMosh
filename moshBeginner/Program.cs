56cb13347909cf0a43aca0f837e47ecbb39da573
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-14 08:20:54 -0400

    created an algorithim to find all divisble values by three up to one hundred

--- moshBeginner/Program.cs
+++ moshBeginner/Program.cs
@@ -1,4 +1,5 @@
-﻿//1- Write a program and ask the user to enter a number.The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console.Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+﻿//PROBLEM SET 1
+//1- Write a program and ask the user to enter a number.The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console.Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
 //using System;
 
 //class MainClass
@@ -121,6 +122,50 @@
 //program should display Ok on the console.If the value is above the speed limit, the program should calculate the number of demerit points.For every 5km/hr above the speed limit,
 //1 demerit points should be incurred and displayed on the console. If the number of demerit points is above 12, the program should display License Suspended.
 
+//using System;
+
+//class MainClass
+//{
+
+//    // Main Method 
+//    public static void Main()
+//    {
+
+//        Console.WriteLine("What is the speed limit?");
+//        int speedLimit = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("What is the current speed?");
+//        int currentSpeed = Convert.ToInt32(Console.ReadLine());
+
+
+
+//        if (currentSpeed < speedLimit)
+//        {
+//            Console.WriteLine("Speed OK");
+
+//        }
+//        else if (currentSpeed > speedLimit)
+//        {
+//            int demeritPoints = (currentSpeed - speedLimit) / 5;
+
+//            if (demeritPoints > 12)
+//            {
+//                Console.WriteLine("License Suspended!");
+//            } else if (demeritPoints > 0 && demeritPoints < 12)
+//            {
+//                Console.WriteLine("A penalty of {0} demerit points", demeritPoints);
+
+//            }
+//            else Console.WriteLine("Warning, within 5kph of speed limit");
+
+//        }
+//    }
+//}
+
+
+
+//PROBLEM SET 2
+//1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.Display the count on the console.
 using System;
 
 class MainClass
@@ -129,34 +174,29 @@ class MainClass
     // Main Method 
     public static void Main()
     {
-
-        Console.WriteLine("What is the speed limit?");
-        int speedLimit = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("What is the current speed?");
-        int currentSpeed = Convert.ToInt32(Console.ReadLine());
-
-
-
-        if (currentSpeed < speedLimit)
+        int count = 0;
+    for (int i = 1; i <= 100; i++)
         {
-            Console.WriteLine("Speed OK");
-
-        }
-        else if (currentSpeed > speedLimit)
-        {
-            int demeritPoints = (currentSpeed - speedLimit) / 5;
-
-            if (demeritPoints > 12)
+            if (i % 3 == 0)
             {
-                Console.WriteLine("License Suspended!");
-            } else if (demeritPoints > 0 && demeritPoints < 12)
-            {
-                Console.WriteLine("A penalty of {0} demerit points", demeritPoints);
-
+                count++;
             }
-            else Console.WriteLine("Warning, within 5kph of speed limit");
-
         }
+        Console.WriteLine(count);
     }
 }
+
+
+//2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+
+
+
+//3- Write a program and ask the user to enter a number.Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+
+
+
+//4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number.If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+
+
+
+//5- Write a program and ask the user to enter a series of numbers separated by comma.Find the maximum of the numbers and display it on the console.For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
