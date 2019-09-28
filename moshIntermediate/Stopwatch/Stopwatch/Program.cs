b356272cbcb0669d5ca48282ad49916a8c9940ef
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-28 13:43:34 -0400

    building a stopWatch app that will be able to return the duration between starts and stops on a stopwatch

---
+++ moshIntermediate/Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public int Start;
+        public int Stop;
+
+        public void SetStart(int start)
+        {
+            Start = start;
+        }
+
+        public void SetStop(int stop)
+        {
+            Stop = stop;
+        }
+
+        public int Duration()
+        {
+            int duration = Stop - Start;
+            return duration;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.SetStart(3);
+            stopwatch.SetStop(5);
+
+            Console.WriteLine(stopwatch.Duration());
+        }
+    }
+}
