8737b03dca01497641b81fb145385e97298503ae
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-28 14:50:00 -0400

    building a class for a stack ovberflow post. User will be able to upvote or down vote the post, and view the current vote total

---
+++ moshIntermediate/StackOverflowPost/StackOverflowPost/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Post
+    {
+        public string Title;
+        public string Description;
+        public int TotalVotes;
+        public DateTime Created;
+
+
+        public void UpVote()
+        {
+            TotalVotes++;
+        }
+
+        public void DownVote()
+        {
+            TotalVotes--;
+        }
+
+        public void CurrentValue()
+        {
+            Console.WriteLine(TotalVotes);
+        }
+
+    }
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var post = new Post();
+
+            post.Title = "Hello World";
+            post.Description = "First program";
+            post.Created = new DateTime(2019, 6, 22, 18, 47, 0);
+
+            Console.WriteLine(post.Created);
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+
+            post.CurrentValue();
+        }
+    }
+}
