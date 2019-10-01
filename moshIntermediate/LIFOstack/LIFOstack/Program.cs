ba4b4b8baae7fa3e4960fc9e6c04a33114eb37a5
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-01 11:42:00 -0400

    succesfully pushing items into the stack

--- moshIntermediate/LIFOstack/LIFOstack/Program.cs
+++ moshIntermediate/LIFOstack/LIFOstack/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace LIFOstack
 {
+    public class Stack
+    {
+        List<int> myList = new List<int>();
+
+        public List<int> Push(object obj)
+        {
+            int i = (int)obj;
+
+            myList.Add(i);
+
+            return myList;
+            //foreach (int item in myarray)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+
+        //public object obj Pop()
+        //{
+        //    myarray.Remove(myarray.Count);
+        //    return;
+        //}
+
+        //public void clear()
+        //{
+
+        //}
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack();
+            stack.Push(3);
+            stack.Push(7);
+            var myList = stack.Push(9);
+
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
